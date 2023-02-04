86003f29cd5f345a5921548765d87a26eee12b0e
niehusst <liamniehusstaab@gmail.com>
2023-02-03 20:28:34 -0700

    stones are captured during play!!!

--- frontend/src/Model/Game.elm
+++ frontend/src/Model/Game.elm
@@ -46,13 +46,3 @@ of how the moves were actually played.
 addMoveToHistory : Move -> Game -> Game
 addMoveToHistory move game =
     { game | history = move :: game.history }
-
-
-playMove : Move -> Game -> Game
-playMove move game =
-    case move of
-        Pass ->
-            setLastMove move (addMoveToHistory move game)
-
-        Play piece position ->
-            setLastMove move (addMoveToHistory move (setBoard (setPieceAt position piece game.board) game))
