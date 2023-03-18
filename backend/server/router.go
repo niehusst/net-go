995f10c7a9ca21a4272c718bcd55ceb20edc127e
niehusst <liamniehusstaab@gmail.com>
2023-03-17 18:50:18 -0600

    update server launch/quit following gin example

--- backend/server/router.go
+++ backend/server/router.go
@@ -6,25 +6,25 @@ import (
 )
 
 func GetRouter() *gin.Engine {
-  router := gin.Default()
+	router := gin.Default()
 
-  router.RedirectTrailingSlash = true
+	router.RedirectTrailingSlash = true
 
-  // load HTML files from glob pattern so gin can reference them
-  router.LoadHTMLGlob("frontend/templates/*")
-  // load the asset files
-  router.Static("/static", "frontend/static")
+	// load HTML files from glob pattern so gin can reference them
+	router.LoadHTMLGlob("frontend/templates/*")
+	// load the asset files
+	router.Static("/static", "frontend/static")
 
-  // API request routes
-//  router.GET("/", func(c *gin.Context) {
-//    c.HTML(http.StatusOK, "index.html", gin.H{})
-//  })
+	// API request routes
+	//  router.GET("/", func(c *gin.Context) {
+	//    c.HTML(http.StatusOK, "index.html", gin.H{})
+	//  })
 
-  // serve the Elm app HTML for any other route; the
-  // app will handle its own routing internally
-  router.NoRoute(func(c *gin.Context) {
+	// serve the Elm app HTML for any other route; the
+	// app will handle its own routing internally
+	router.NoRoute(func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{})
-  })
+	})
 
-  return router
+	return router
 }
--- main.go
+++ main.go
@@ -1,12 +1,53 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"net-go/server/backend/server"
 )
 
 func main() {
-	// TODO: refactor this code into server.go, only call Start from here (and pass in env settings?)
+	log.Println("Starting server...")
 	router := server.GetRouter()
 
-	router.Run(":4000")
+	srv := &http.Server{
+		Addr:    ":4000",
+		Handler: router,
+	}
+
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	// https://github.com/gin-gonic/examples/blob/master/graceful-shutdown/graceful-shutdown/notify-without-context/server.go
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// this blocks until `quit` channel receives a kill signal
+	<-quit
+	log.Println("Shutting down server...")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
+
+	//	router.Run(":4000")
 }
