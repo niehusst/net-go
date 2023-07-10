dcf34e3f09a94a7d36f6191c21ea084b221bb5f9
niehusst <liamniehusstaab@gmail.com>
2023-07-09 21:22:36 -0600

    really ignore elm.js now

--- frontend/static/js/elm.js
+++
@@ -1,2 +0,0 @@
-// placeholder js file. compile elm app to populate w/ real data.
-// Leaving this as a placeholder rather than committing functional file bcus i dont want js to be dominant project language on gh
