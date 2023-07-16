c0c43b4c09dd403eda451bae6b159a0e211a0fac
niehusst <liamniehusstaab@gmail.com>
2023-07-15 21:45:50 -0600

    feat: net req to signup + password hashing

--- frontend/src/Page/SignUp.elm
+++ frontend/src/Page/SignUp.elm
@@ -3,21 +3,40 @@ module Page.SignUp exposing (Model, Msg, init, update, view)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
+import Http
+import Json.Decode as Decode
+import Json.Decode.Pipeline
+import Json.Encode as Encode
+import RemoteData exposing (RemoteData, WebData)
+import Sha256 exposing (sha256)
 
 
 type alias Model =
     { username : String
     , password : String
     , confirmPassword : String
-    , errorBannerText : Maybe String
+    , formResponse : WebData SignupResponseData
     }
 
 
 type Msg
-    = Signup
-    | SaveUsername String
+    = SaveUsername String
     | SavePassword String
     | SaveConfirmPassword String
+    | SendHttpSignupReq
+    | ReceiveHttpSignupResp (WebData SignupResponseData)
+
+
+type alias SignupResponseData =
+    { ok : Bool
+    }
+
+
+type alias SignupRequestData r =
+    { r
+        | username : String
+        , password : String
+    }
 
 
 
@@ -28,20 +47,42 @@ view : Model -> Html Msg
 view model =
     div [ class "TODO css here" ]
         [ h1 [] [ text "Sign Up" ]
-        , viewBanner model
-        , viewForm model
+        , viewBody model
         ]
 
 
-viewBanner : Model -> Html Msg
-viewBanner model =
-    case model.errorBannerText of
-        Nothing ->
-            div [] []
+viewBody : Model -> Html Msg
+viewBody model =
+    case model.formResponse of
+        RemoteData.NotAsked ->
+            div []
+                [ viewForm model ]
 
-        Just bannerText ->
-            div [ style "color" "red" ]
-                [ text <| "Error: " ++ bannerText ]
+        RemoteData.Loading ->
+            -- TODO: spinner or something + css
+            div []
+                [ text "Loading..." ]
+
+        RemoteData.Success msg ->
+            -- this will likely never be shown
+            div []
+                [ text "Signup Success!" ]
+
+        RemoteData.Failure error ->
+            div []
+                [ viewForm model
+                , viewBanner error
+                ]
+
+
+viewBanner : Http.Error -> Html Msg
+viewBanner error =
+    let
+        errString =
+            stringFromHttpError error
+    in
+    div [ style "color" "red" ]
+        [ text <| "Error: " ++ errString ]
 
 
 viewForm : Model -> Html Msg
@@ -75,7 +116,7 @@ viewForm model =
                 []
             ]
         , div []
-            [ button [ type_ "submit", onClick Signup ]
+            [ button [ type_ "submit", onClick SendHttpSignupReq ]
                 [ text "Create Account" ]
             ]
         ]
@@ -83,6 +124,58 @@ viewForm model =
 
 
 -- UPDATE --
+
+
+stringFromHttpError : Http.Error -> String
+stringFromHttpError error =
+    case error of
+        Http.BadUrl msg ->
+            msg
+
+        Http.Timeout ->
+            "Server took too long to repond; try again later"
+
+        Http.NetworkError ->
+            "Network error; try again later"
+
+        Http.BadStatus errCode ->
+            if errCode == 500 then
+                "Internal server error :("
+
+            else
+                "Failed to signup for that username and password. Note: passwords must be between 8 and 30 characters."
+
+        Http.BadBody msg ->
+            msg
+
+
+signupDecoder : Decode.Decoder SignupResponseData
+signupDecoder =
+    Decode.succeed SignupResponseData
+        |> Json.Decode.Pipeline.required "ok" Decode.bool
+
+
+signupEncoder : SignupRequestData r -> Encode.Value
+signupEncoder reqData =
+    Encode.object
+        [ ( "username", Encode.string reqData.username )
+        , ( "password", Encode.string reqData.password )
+        ]
+
+
+sendSignupReq : SignupRequestData r -> Cmd Msg
+sendSignupReq reqData =
+    let
+        hashedReqData =
+            { reqData | password = sha256 reqData.password }
+    in
+    Http.post
+        { url = "/api/signup"
+        , body = Http.jsonBody (signupEncoder hashedReqData)
+        , expect =
+            signupDecoder
+                |> Http.expectJson (RemoteData.fromResult >> ReceiveHttpSignupResp)
+        }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -103,17 +196,28 @@ update msg model =
             , Cmd.none
             )
 
-        Signup ->
+        SendHttpSignupReq ->
             if model.password /= model.confirmPassword then
-                ( { model | errorBannerText = Just "Passwords don't match!" }
+                ( { model | formResponse = RemoteData.Failure (Http.BadBody "Passwords don't match!") }
                 , Cmd.none
                 )
 
             else
-                -- TODO: network req
-                ( { model | errorBannerText = Nothing }
-                , Cmd.none
+                ( { model | formResponse = RemoteData.Loading }
+                , sendSignupReq model
                 )
+
+        --        ReceiveHttpSignupResp (RemoteData.Success _) ->
+        --            -- TODO: save auth state somewhere
+        --            -- TODO: nav to home page or somethign
+        --            ( model
+        --            , Cmd.none
+        --            )
+        ReceiveHttpSignupResp response ->
+            -- fallthrough catch other RemoteData states
+            ( { model | formResponse = response }
+            , Cmd.none
+            )
 
 
 
@@ -132,5 +236,5 @@ initialModel =
     { username = ""
     , password = ""
     , confirmPassword = ""
-    , errorBannerText = Nothing
+    , formResponse = RemoteData.NotAsked
     }
