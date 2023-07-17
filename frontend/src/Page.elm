6cd153498447f036c97de70057cffa73738950af
niehusst <liamniehusstaab@gmail.com>
2023-07-16 22:51:43 -0600

    remove unused junk

--- frontend/src/Page.elm
+++ frontend/src/Page.elm
@@ -1,8 +1,7 @@
-module Page exposing (viewErrors, viewFooter, viewHeader)
+module Page exposing (viewFooter, viewHeader)
 
 -- yoinked from https://github.com/rtfeldman/elm-spa-example/blob/master/src/Page.elm
 
-import Browser exposing (Document)
 import Html exposing (Html, a, button, div, footer, i, img, li, nav, p, span, text, ul)
 import Html.Attributes exposing (class, classList, href, style)
 import Html.Events exposing (onClick)
@@ -57,24 +56,3 @@ linkTo : Route -> List (Html msg) -> Html msg
 linkTo route linkContent =
     li [ classList [ ( "nav-item", True ) ] ]
         [ a [ class "nav-link", href (routeToString route) ] linkContent ]
-
-
-{-| Render dismissable errors. We use this all over the place!
--}
-viewErrors : msg -> List String -> Html msg
-viewErrors dismissErrors errors =
-    if List.isEmpty errors then
-        Html.text ""
-
-    else
-        div
-            [ class "error-messages"
-            , style "position" "fixed"
-            , style "top" "0"
-            , style "background" "rgb(250, 250, 250)"
-            , style "padding" "20px"
-            , style "border" "1px solid"
-            ]
-        <|
-            List.map (\error -> p [] [ text error ]) errors
-                ++ [ button [ onClick dismissErrors ] [ text "Ok" ] ]
