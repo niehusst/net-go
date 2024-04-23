d6946c87e2550701154685d1767476bd04fd0089
niehusst <liamniehusstaab@gmail.com>
2024-04-22 22:18:26 -0700

    test: isActiveTurn logic

---
+++ frontend/tests/GameTest.elm
@@ -0,0 +1,69 @@
+module GameTest exposing (..)
+
+
+import Expect exposing (Expectation)
+import Model.Game exposing (..)
+import Model.Board exposing (BoardSize(..))
+import Model.Piece exposing (Piece(..))
+import Model.Move exposing (Move(..))
+import Model.ColorChoice exposing (ColorChoice(..))
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "Game model"
+        [ describe "isActiveTurn"
+            [ test "black player has active turn in new game" <|
+                \_ ->
+                    let
+                        game =
+                            newGame Full Black 0.0
+                    in
+                    Expect.equal True (isActiveTurn game)
+            , test "white player not active turn in new game" <|
+                \_ ->
+                    let
+                        game =
+                            newGame Full White 0.0
+                    in
+                    Expect.equal False (isActiveTurn game)
+            , test "game with history determines correct active turn" <|
+                \_ ->
+                    let
+                        game =
+                            newGame Full Black 0.0
+                                |> addMoveToHistory (Play BlackStone 0)
+                                |> addMoveToHistory (Play WhiteStone 1)
+                                |> addMoveToHistory (Play BlackStone 5)
+                    in
+                    Expect.equal False (isActiveTurn game)
+            , test "active turn correct when last move was pass" <|
+                \_ ->
+                    let
+                        game =
+                            newGame Full Black 0.0
+                                |> addMoveToHistory (Play BlackStone 0)
+                                |> addMoveToHistory (Pass WhiteStone)
+                    in
+                    Expect.equal True (isActiveTurn game)
+            , test "active turn correct when 2 passes in a row" <|
+                \_ ->
+                    let
+                        game =
+                            newGame Full White 0.0
+                                |> addMoveToHistory (Play BlackStone 0)
+                                |> addMoveToHistory (Pass WhiteStone)
+                                |> addMoveToHistory (Pass BlackStone)
+                    in
+                    Expect.equal True (isActiveTurn game)
+            , test "active turn correct when first move was pass" <|
+                \_ ->
+                    let
+                        game =
+                            newGame Full White 0.0
+                                |> addMoveToHistory (Pass BlackStone)
+                    in
+                    Expect.equal True (isActiveTurn game)
+            ]
+        ]
