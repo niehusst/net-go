409de21b455d5e2dc00faaf7eeaa0b88a27da9db
niehusst <liamniehusstaab@gmail.com>
2025-02-05 21:14:07 -0800

    add update game api endpoint

--- backend/handler/router/endpoints/game.go
+++ backend/handler/router/endpoints/game.go
@@ -19,7 +19,11 @@ type createGameRequest struct {
 	Game ElmGame `json:"game" binding:"required"`
 }
 
-func parseUriParams(c *gin.Context) (*gameUri, error) {
+type updateGameRequest struct {
+	Game ElmGame `json:"game" binding:"required"`
+}
+
+func parseGameIdUriParam(c *gin.Context) (*gameUri, error) {
 	// bind uri params
 	var uriParams gameUri
 	if err := c.ShouldBindUri(&uriParams); err != nil {
@@ -40,42 +44,6 @@ func getUserFromCtx(c *gin.Context) (*model.User, error) {
 	}
 	user := untypedUser.(*model.User)
 	return user, nil
-}
-
-// GET /:id
-func (rhandler RouteHandler) GetGame(c *gin.Context) {
-	// make sure we got authed user
-	user, err := getUserFromCtx(c)
-	if err != nil {
-		log.Printf("Expected to have authed user from middleware, but found none\n")
-		c.JSON(apperrors.Status(err), gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	uriParams, err := parseUriParams(c)
-	if err != nil {
-		// JSON resp handled in helper func failure
-		return
-	}
-
-	game, err := rhandler.Provider.GameService.Get(c, uriParams.ID)
-	if err != nil {
-		log.Printf("Error fetching game with id %d: %v\n", uriParams.ID, err)
-		notFoundErr := apperrors.NewNotFound("Game", strconv.FormatUint(uint64(uriParams.ID), 10))
-		c.JSON(notFoundErr.Status(), gin.H{
-			"error": notFoundErr.Error(),
-		})
-		return
-	}
-
-	// return game in shape elm expects
-	var respGame ElmGame
-	respGame.fromGame(*game, *user)
-	c.JSON(http.StatusOK, gin.H{
-		"game": respGame,
-	})
 }
 
 // this follows the definition of Game in the Elm frontend
@@ -142,6 +110,46 @@ func (r *ElmGame) fromGame(g model.Game, authedUser model.User) {
 	}
 }
 
+func validateUserIsPlayerInGame(game model.Game, user model.User) bool {
+	return user.ID == game.BlackPlayerId || user.ID == game.WhitePlayerId
+}
+
+// GET /:id
+func (rhandler RouteHandler) GetGame(c *gin.Context) {
+	// make sure we got authed user
+	user, err := getUserFromCtx(c)
+	if err != nil {
+		log.Printf("Expected to have authed user from middleware, but found none\n")
+		c.JSON(apperrors.Status(err), gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	uriParams, err := parseGameIdUriParam(c)
+	if err != nil {
+		// JSON resp handled in helper func failure
+		return
+	}
+
+	game, err := rhandler.Provider.GameService.Get(c, uriParams.ID)
+	if err != nil {
+		log.Printf("Error fetching game with id %d: %v\n", uriParams.ID, err)
+		notFoundErr := apperrors.NewNotFound("Game", strconv.FormatUint(uint64(uriParams.ID), 10))
+		c.JSON(notFoundErr.Status(), gin.H{
+			"error": notFoundErr.Error(),
+		})
+		return
+	}
+
+	// return game in shape elm expects
+	var respGame ElmGame
+	respGame.fromGame(*game, *user)
+	c.JSON(http.StatusOK, gin.H{
+		"game": respGame,
+	})
+}
+
 // POST /
 func (rhandler RouteHandler) CreateGame(c *gin.Context) {
 	// make sure we got authed user
@@ -188,5 +196,73 @@ func (rhandler RouteHandler) CreateGame(c *gin.Context) {
 
 	c.JSON(http.StatusCreated, gin.H{
 		"uid": game.ID,
+	})
+}
+
+// POST /:id
+func (rhandler RouteHandler) UpdateGame(c *gin.Context) {
+	uriParams, err := parseGameIdUriParam(c)
+	if err != nil {
+		// JSON resp handled in helper func failure
+		return
+	}
+
+	// make sure we got authed user
+	user, err := getUserFromCtx(c)
+	if err != nil {
+		log.Printf("Expected to have authed user from middleware, but found none\n")
+		c.JSON(apperrors.Status(err), gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	var req updateGameRequest
+	if ok := binding.BindData(c, &req); !ok {
+		// bind failure resp handled in BindData
+		return
+	}
+
+	// transform input into game model
+	game, err := req.Game.toGame(user)
+	if err != nil {
+		log.Printf("Failed to construct game model from request data\n")
+		c.JSON(apperrors.Status(err), gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// fetch current value from DB
+	currentGame, err := rhandler.Provider.GameService.Get(c, uriParams.ID)
+	if err != nil {
+		log.Printf("Error fetching game with id %d: %v\n", uriParams.ID, err)
+		notFoundErr := apperrors.NewNotFound("Game", strconv.FormatUint(uint64(uriParams.ID), 10))
+		c.JSON(notFoundErr.Status(), gin.H{
+			"error": notFoundErr.Error(),
+		})
+		return
+	}
+
+	// validate the authed user has correct ownership to update the game
+	if !validateUserIsPlayerInGame(*currentGame, *user) {
+		forbiddenError := apperrors.NewForbidden()
+		c.JSON(forbiddenError.Status(), gin.H{
+			"error": forbiddenError.Error(),
+		})
+	}
+
+	if err := rhandler.Provider.GameService.Update(c, game); err != nil {
+		c.JSON(apperrors.Status(err), gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// return game in shape elm expects
+	var respGame ElmGame
+	respGame.fromGame(*game, *user)
+	c.JSON(http.StatusOK, gin.H{
+		"game": respGame,
 	})
 }
