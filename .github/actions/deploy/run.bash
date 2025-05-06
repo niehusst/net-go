7a73103fec5f6b382ca96558c81bc543cc853288
niehusst <liamniehusstaab@gmail.com>
2025-05-06 10:19:51 -0700

    ci: cleanse docker tag cache before deploy

--- .github/actions/deploy/run.bash
+++ .github/actions/deploy/run.bash
@@ -6,7 +6,15 @@ echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519
 echo "$SSH_KNOWN_HOSTS_B64" | base64 -d  > ~/.ssh/known_hosts
 
 # run docker restart over ssh
-ssh $SSH_USER@$SSH_HOST "echo '$AUTH_TOKEN' | docker login ghcr.io -u $DOCKER_USER --password-stdin && docker pull ghcr.io/niehusst/net-go:$TAG_NAME ; docker stop net_go ; docker rm net_go ; docker run -d --name net_go -p 8080:8080 -v netgo_db:/root ghcr.io/niehusst/net-go:$TAG_NAME && exit"
+IMAGE_NAME="ghcr.io/niehusst/net-go"
+CONTAINER_NAME="net_go"
+ssh $SSH_USER@$SSH_HOST "echo '$AUTH_TOKEN' | docker login ghcr.io -u $DOCKER_USER --password-stdin && 
+docker stop $CONTAINER_NAME && 
+docker rm $CONTAINER_NAME && 
+docker image prune -af && 
+docker pull $IMAGE_NAME:$TAG_NAME && 
+docker run -d --name $CONTAINER_NAME -p 8080:8080 -v netgo_db:/root $IMAGE_NAME:$TAG_NAME && 
+exit"
 
 # cleanup
 rm -rf ~/.ssh
