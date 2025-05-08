781fe8b2428705775a818f39d0936724b6687d01
niehusst <liamniehusstaab@gmail.com>
2025-05-07 21:13:24 -0700

    ci: update deploy script to use docker compose

--- .github/actions/deploy/run.bash
+++ .github/actions/deploy/run.bash
@@ -3,17 +3,24 @@
 # setup SSH key auth
 install -m 600 -D /dev/null ~/.ssh/id_ed25519
 echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519
-echo "$SSH_KNOWN_HOSTS_B64" | base64 -d  > ~/.ssh/known_hosts
+echo "$SSH_KNOWN_HOSTS_B64" | base64 -d > ~/.ssh/known_hosts
+
+# copy (possibly) new version of docker compose file over + secrets to spin it up
+echo "$ENV_PROD_B64" | base64 -d > .env
+scp compose.yaml $SSH_HOST:/home/$SSH_USER/new-compose.yaml
+scp .env $SSH_HOST:/home/$SSH_USER/.env.new
 
 # run docker restart over ssh
 IMAGE_NAME="ghcr.io/niehusst/net-go"
 CONTAINER_NAME="net_go"
 ssh $SSH_USER@$SSH_HOST "echo '$AUTH_TOKEN' | docker login ghcr.io -u $DOCKER_USER --password-stdin && 
-docker stop $CONTAINER_NAME && 
-docker rm $CONTAINER_NAME && 
+docker compose down && 
 docker image prune -af && 
 docker pull $IMAGE_NAME:$TAG_NAME && 
-docker run -d --name $CONTAINER_NAME -p 8080:8080 -v netgo_db:/root $IMAGE_NAME:$TAG_NAME && 
+mv new-compose.yaml compose.yaml && 
+mv .env.new .env && 
+echo 'NETGO_IMAGE=$IMAGE_NAME:$TAG_NAME' >> .env && 
+docker compose up -d && 
 exit"
 
 # cleanup
