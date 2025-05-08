1d634c5e1926cd4e96f05ff83eb56d8ce420ba9c
niehusst <liamniehusstaab@gmail.com>
2025-05-07 23:16:03 -0700

    feat: slightly more css improvment

--- frontend/src/Page/NotFound.elm
+++ frontend/src/Page/NotFound.elm
@@ -11,8 +11,8 @@ import Route exposing (Route, routeToString)
 
 view : Html msg
 view =
-    div [ class "flex flex-col items-center justify-center" ]
-        [ h1 [] [ text "404" ]
+    div [ class "flex flex-col items-center justify-center gap-3 my-6" ]
+        [ h1 [ class "text-3xl font-black" ] [ text "404" ]
         , h3 [] [ text "Oops! This page could not be found." ]
         , a [ href (routeToString Route.Home) ]
             [ button [ class "btn" ] [ text "Return to home page" ] ]
